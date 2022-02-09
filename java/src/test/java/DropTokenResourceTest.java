ff69c26c5c18ba7b5e2df02dacb8e563876c1598
Jay Harkar <jayharkar@Jamies-MacBook-Pro-2.local>
2022-02-09 00:24:53 -0500

    save

---
+++ java/src/test/java/DropTokenResourceTest.java
@@ -0,0 +1,41 @@
+//import org.glassfish.jersey.test.JerseyTest;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
+//
+//import javax.ws.rs.core.Application;
+//import javax.ws.rs.core.HttpHeaders;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//
+//import org.glassfish.jersey.server.ResourceConfig;
+//import org.glassfish.jersey.test.JerseyTest;
+//import org.junit.Test;
+//
+//import com._98point6.droptoken.DropTokenResource;
+//
+//public class DropTokenResourceTest extends JerseyTest {
+public class DropTokenResourceTest {
+//    @Override
+//    protected Application configure() {
+//        return new ResourceConfig(DropTokenResource.class);
+//    }
+//    
+//    @Test
+//    public void whenThis_thenThat() {
+//        assertTrue(true);
+//    }
+//    
+////    @Test
+////    public void getGamesTest() {
+////        Response response = target("/drop_token").request()
+////            .get();
+////
+////        //assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+////        assertEquals("Http Content-Type should be: ", MediaType.APPLICATION_JSON, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+////
+////        String content = response.readEntity(String.class);
+////        //assertEquals("Content of response is: ", "hi", content);
+////    }
+//    
+}
